6ce1a1328d934610ac18274aa7f5562aa1064dc0
leto-silva <wellingtoneugenio@gmail.com>
2021-02-25 17:18:41 -0300

    Inclusão fun pessoaInfo

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,13 +5,15 @@ class Pessoa {
 
     var cpf:String = "123.123.123-23"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$name - $cpf"
 }
 
 fun main(){
     var jetter = Pessoa()
     //jetter.cpf ="123589E"
 
-    println(jetter.name)
-    println(jetter.cpf)
-
+    println(jetter.pessoaInfo())
 }
