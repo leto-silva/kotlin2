ae1a9e3d114db759808d27e2f856a5f940a697e1
leto-silva <wellingtoneugenio@gmail.com>
2021-02-25 16:54:40 -0300

    Modificação com private set Objeto Pessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,18 +2,16 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var name:String = "Jetter"
-    var cpf:String = "123.123.123-23"
 
-    inner class Endereco{
-        var rua = "rua teste"
-    }
+    var cpf:String = "123.123.123-23"
+    private set
 }
 
 fun main(){
     var jetter = Pessoa()
+    //jetter.cpf ="123589E"
 
     println(jetter.name)
     println(jetter.cpf)
 
-    println(jetter.Endereco().rua)
 }
