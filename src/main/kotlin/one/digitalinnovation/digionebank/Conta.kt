eda361d5ea3ec437347f64d90645983ee4042773
leto-silva <wellingtoneugenio@gmail.com>
2021-02-26 10:29:49 -0300

    Created class Conta

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Conta {
+    val agencia: String
+    val numero: String
+    val saldo: BigDecimal
+
+    fun Deposito(valor: BigDecimal){
+
+    }
+
+    fun Saque(valor: BigDecimal){
+
+    }
+
+}
